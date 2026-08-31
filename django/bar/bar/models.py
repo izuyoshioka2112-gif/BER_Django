e28f6c3068547c6ce57bc64859ca116c1556f436
Suiha <izusugijamu@gmail.com>
2026-08-31 16:54:32 +0900

    データを追加

--- django/bar/bar/models.py
+++ django/bar/bar/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class  Product(models. Model):
+     name = models .CharField(max_length=100)
+     price = models.IntegerField(max_length= 500)
+     category = models.CharField(max_length=10, default='drink') 
+
+     def __str__(self):
+        return self.name
+
+ 
