ce425a0f3579ce536b0d8e7a38ce4bd05baea5da
Suiha <izusugijamu@gmail.com>
2026-08-31 20:26:25 +0900

    テーブル設定し直し

--- django/bar/bar/models.py
+++ django/bar/bar/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+CATEGORY = (('snack'), ' スナック'), (('drink'), 'ドリンク')
 class  Product(models. Model):
      name = models .CharField(max_length=100)
-     price = models.IntegerField(max_length= 500)
-     category = models.CharField(max_length=10, default='drink') 
+     price = models.IntegerField()
+     category = models.CharField(
+          max_length=100,
+          choices = CATEGORY
+          )
 
      def __str__(self):
         return self.name
